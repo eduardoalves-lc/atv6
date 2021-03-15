55d97783f1a4f479327d977dfc937eb7809421fb
Eduardo Alves <54808848+eduardoalves-lc@users.noreply.github.com>
2021-03-14 21:13:32 -0300

    atv6

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Firebase from './components';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}><Firebase /></SafeAreaView>
+  );
+};
+
+export default App;
